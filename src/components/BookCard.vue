d183e1d7ddfd0c4c352a1ad84e99b82c7bb5be2f
BayuDC <dev.bayudc@gmail.com>
2022-03-14 12:04:11 +0700

    Add bookcard component

---
+++ src/components/BookCard.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "BookCard",
+    props: ["title", "author"],
+};
+</script>
+<template>
+    <div class="card border-dark">
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <h6 class="card-subtitle mb-2">{{ author }}</h6>
+        </div>
+    </div>
+</template>
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,12 +1,23 @@
 <script>
+import BookCard from "./BookCard.vue";
+
 export default {
     name: "BookList",
     props: ["books"],
+    components: {
+        BookCard,
+    },
 };
 </script>
 
 <template>
-    <ul>
-        <li v-for="book in books" :key="book.id">{{ book.title }}</li>
-    </ul>
+    <div class="row">
+        <div
+            v-for="book in books"
+            :key="book.id"
+            class="col-md-3 col-sm-4 col-12"
+        >
+            <BookCard :title="book.title" :author="book.author" />
+        </div>
+    </div>
 </template>
