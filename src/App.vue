38c92e708c3bfe443e0b91d2a187573f817092b6
BayuDC <dev.bayudc@gmail.com>
2022-03-14 11:30:42 +0700

    Add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+    name: "App",
+    components: {
+        Navbar,
+    },
+};
 </script>
 
 <template>
+    <div class="mb-4">
+        <Navbar />
+    </div>
+    <div class="container">
+        <h1>Hello World</h1>
+    </div>
 </template>
-
-<style>
-</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "Navbar",
+};
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container">
+            <a class="navbar-brand text-info" href="#">Pervues</a>
+        </div>
+    </nav>
+</template>
