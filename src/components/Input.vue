4e95f7228278df9a78be35f09d5987de2bd0c4c4
BayuDC <dev.bayudc@gmail.com>
2022-03-15 13:42:12 +0700

    Add input component

---
+++ src/components/Input.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'Input',
+    props: ['name'],
+};
+</script>
+
+<template>
+    <div class="mb-2">
+        <label :for="name" class="form-labell">{{ name }}</label>
+        <input type="text" :id="name" class="form-control" required />
+    </div>
+</template>
